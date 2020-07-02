432179348eaadd20056d92b39b079b31f16e9949
Nawodya Jayalath <nawodyahckr@gmail.com>
2020-07-02 22:10:56 +0530

    first row added

--- lib/home.dart
+++ lib/home.dart
@@ -3,14 +3,24 @@ import 'package:flutter/material.dart';
 class Home extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
     return _homeState();
   }
 }
 
 class _homeState extends State<Home> {
-  @override 
-  Widget build(BuildContext context){
-    return 
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Text('Covid-19 \n Tracker'),
+          ),
+          Expanded(
+            child: Image.asset('flag.jpg'),
+          ),
+        ],
+      ),
+    );
   }
 }
