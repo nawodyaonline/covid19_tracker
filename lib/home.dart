a1d6e1d5c56e7f1930566c96ffb787882ae956dd
Nawodya Jayalath <nawodyahckr@gmail.com>
2020-07-02 22:08:34 +0530

    image files & separearate class added

---
+++ lib/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _homeState();
+  }
+}
+
+class _homeState extends State<Home> {
+  @override 
+  Widget build(BuildContext context){
+    return 
+  }
+}
